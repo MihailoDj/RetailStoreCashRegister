66ca9c15eead5d7f1ecf2dc48575bfde61de937d
Mihailo Djordjevic <mihailodjordjevic392@gmail.com>
2022-03-07 12:18:08 +0100

    Adding a main form controller.

---
+++ Controllers/MainController.cs
@@ -0,0 +1,36 @@
+﻿using RetailStoreCashRegister.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailStoreCashRegister.Controllers
+{
+    public class MainController
+    {
+        private MainForm _form;
+
+        public MainController(MainForm form)
+        {
+            _form = form;
+            AddActionListeners();
+        }
+
+        private void AddActionListeners()
+        {
+            _form.GetAdminAndStatsButton().Click += new EventHandler(OpenAdminForm);
+            _form.GetSalesAndBillingButton().Click += new EventHandler(OpenSalesForm);
+        }
+
+        private void OpenSalesForm(object sender, EventArgs e)
+        {
+            Coordinator.GetInstance().OpenSalesForm();
+        }
+
+        private void OpenAdminForm(object sender, EventArgs e)
+        {
+            Coordinator.GetInstance().OpenAdministrationForm();
+        }
+    }
+}
